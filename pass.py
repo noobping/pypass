cecae23161a41e60284ed9e072e49e435f6cf533
nick <noobping@users.noreply.github.com>
2023-08-12 09:37:21 +0200

    no search, but filter

--- pass.py
+++ pass.py
@@ -12,6 +12,58 @@ class PassWrapper:
     def __init__(self):
         pass
 
+    def process_passwords_tree(self, folder='.', query=None, filter_valid_files=False):
+        if query:
+            command = ['pass', 'find', query]
+        else:
+            command = ['pass', 'ls', folder]
+            
+        result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        output = result.stdout.decode('utf-8') if result.returncode == 0 else None
+        if output is None:
+            return None
+
+        # Remove ANSI escape codes
+        ansi_escape = re.compile(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])')
+        output = ansi_escape.sub('', output)
+
+        lines = output.split('\n')[1:]  # Skip the first line
+        children = []
+        current_indent = None
+        current_path = []
+        password_store_path = os.path.expanduser('~/.password-store') # Default password store path
+        for line in lines:
+            stripped_line = line.lstrip()
+            indent = len(line) - len(stripped_line)
+            if current_indent is None:
+                current_indent = indent
+            if indent == current_indent and (stripped_line.startswith("├──") or stripped_line.startswith("└──")):
+                item_name = stripped_line.replace("├──", "").replace("└──", "").strip()
+                if folder != '.':
+                    item_path = os.path.join(password_store_path, folder.lstrip('.'), item_name)
+                else:
+                    item_path = os.path.join(password_store_path, item_name)
+                    
+                if filter_valid_files:
+                    file_command = ['file', item_path]
+                    file_result = subprocess.run(file_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                    file_output = file_result.stdout.decode('utf-8') if file_result.returncode == 0 else None
+                    if file_output and ("PGP RSA encrypted session key" in file_output or "directory" in file_output):
+                        children.append(item_name)
+                else:
+                    children.append(item_name)
+            elif indent < current_indent:
+                break
+            elif query and stripped_line.endswith("/"):
+                # Update the current path for search results
+                current_path.append(stripped_line.rstrip("/"))
+            elif query and stripped_line and current_path:
+                # Append the item to the current path for search results
+                full_path = os.path.join(*current_path, stripped_line)
+                children.append(full_path)
+                
+        return children
+
     def list_passwords(self, folder='.', filter_valid_files=False):
         command = ['pass', 'ls', folder]
         result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -48,21 +100,10 @@ class PassWrapper:
                 break
         return children
 
-    def get_folder_contents(self, folder):
-        # List all passwords and folders recursively
-        password_tree = self.list_passwords(folder)
-        return password_tree
-
     def show_password(self, path):
         command = ['pass', 'show', path]
         result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         return result.stdout.decode('utf-8') if result.returncode == 0 else None
-
-    def find_passwords(self, query):
-        command = ['pass', 'find', query]
-        result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        output = result.stdout.decode('utf-8') if result.returncode == 0 else None
-        return output.splitlines() if output else []
 
     def get_password(self, path):
         command = ['pass', path]
@@ -124,8 +165,19 @@ class PasswordApp(Gtk.ApplicationWindow):
 
     def on_search_clicked(self, button):
         query = self.search_entry.get_text()
-        results = self.pass_manager.find_passwords(query)
-        self.load_folder(results)
+        self.current_folder = '.'
+
+        self.set_title('Password Search')
+        self.back_button.set_visible(True)
+
+        # Remove all children from the list box
+        for row in list(self.list_box):
+            self.list_box.remove(row)
+
+        folder_contents = self.pass_manager.process_passwords_tree(self.current_folder, query, True)
+        for item in folder_contents:
+            label = Gtk.Label(label=item)
+            self.list_box.append(label)
 
     def load_folder(self, folder):
         self.current_folder = folder
@@ -139,7 +191,7 @@ class PasswordApp(Gtk.ApplicationWindow):
         for row in list(self.list_box):
             self.list_box.remove(row)
 
-        folder_contents = self.pass_manager.get_folder_contents(folder)
+        folder_contents = self.pass_manager.list_passwords(folder)
         for item in folder_contents:
             label = Gtk.Label(label=item)
             self.list_box.append(label)
@@ -197,6 +249,10 @@ class PasswordManagerApplication(Gtk.Application):
         win.present()
 
 def main():
+    pass_manager = PassWrapper()
+    password_tree = pass_manager.process_passwords_tree('.', 'nick', True)
+    print(password_tree)
+
     app = PasswordManagerApplication()
     app.run()
 
